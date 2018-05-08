7813f88663a462dfc1381d81455f7abd2f8d4354
rimduhui <>
2018-05-09 01:28:08 +0900

    complete simple login

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^login/$', views.login, name='login'),
     url(r'^$', views.Main.as_view(), name='quiz_main'),
     url(r'^leader/$', views.leader, name='leader'),
     url(r'^result/$', views.result, name='result'),
--- main/views.py
+++ main/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 from .models import Quiz
 
 
+def login(request):
+    return render(request, "main/login.html")
+
+
 class Main(generic.ListView):
     template_name = "main/quiz_main.html"
     context_object_name = "quizs"
 
     def get_queryset(self):
         return Quiz.objects.order_by('number')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(Main, self).get_context_data(**kwargs)
+        context['name'] = self.request.GET
+        return context
 
 
 def leader(request):
